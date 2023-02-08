7002a1e2b9060f3ee8a6566e0631fd45572a78b5
juanpablo1978 <jpandrujovich@gmail.com>
2023-02-08 20:31:42 -0300

    ultimos estilos

--- Desktop/react-card/src/components/FavoriteCard.jsx
+++ Desktop/react-card/src/components/FavoriteCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import {MdDelete } from "react-icons/md"
 
 
 const FavoriteCard = () => {
   return (
-    <main className="bg-slate-900 min-h-screen flex flex-col items-center pt-36">
-        <div className='h-[240px] w-[340px] rounded-[20px] overflow-hidden'>
-          <img className='w-full h-full object-cover' src="public/assets/card.jpg" alt="card"/>
-          <h2 className='text-[30px] font-bold text-white'>TITLE MOVIE</h2>
+    <main className="bg-slate-900 min-h-screen pt-36 flex flex-col items-center">
+        <div className='h-[240px] w-[340px] overflow-hidden relative'>
+          <img className='w-full h-full object-cover rounded-[20px] opacity-70 shadow-2xl' src="public/assets/card.jpg"/>
+          <h3 className='text-[30px] font-bold text-white absolute top-[185px] left-6'>TITLE MOVIE</h3>
+          <div className='absolute top-4 right-4 text-white text-2xl'>
+          <MdDelete/>
+          </div>
         </div>
     </main>
   )
 }
 
+/*QUEDE EN 01:33 APROX DE LA CLASE 05/01/23*/
 
 export default FavoriteCard
 
-/* quede en 50:18 de la clase del 05/01/23*/
